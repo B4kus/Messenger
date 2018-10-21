babda7ea8718f447ff4d3f556989aba304a04039
Szymon Szysz <sszysz@netizens.pl>
2018-10-21 12:24:22 +0200

    Initial commit

---
+++ Source/MainViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Szymon Szysz on 20/10/2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
